ca8d9f676b26251c6a745d277f7dc5c8778e4d27
William Chen <13495049+CPWstatic@users.noreply.github.com>
2022-03-01 16:55:34 +0800

    Impl update handler.

--- nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/operationhandlers/NebulaUpdateOperationHandler.java
+++ nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/operationhandlers/NebulaUpdateOperationHandler.java
@@ -6,11 +6,20 @@ import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcNoResult;
 import com.ldbc.impls.workloads.ldbc.snb.nebula.NebulaDbConnectionState;
 import com.ldbc.impls.workloads.ldbc.snb.operationhandlers.UpdateOperationHandler;
+import com.vesoft.nebula.client.graph.net.Session;
 
 public abstract class NebulaUpdateOperationHandler <TOperation extends Operation<LdbcNoResult>>
         implements UpdateOperationHandler<TOperation, NebulaDbConnectionState> {
     @Override
-    public void executeOperation(TOperation operation, NebulaDbConnectionState dbConnectionState, ResultReporter resultReporter) throws DbException {
-        // TODO:
+    public void executeOperation(TOperation operation, NebulaDbConnectionState state, ResultReporter resultReporter) throws DbException {
+        Session session = state.getSession();
+        try {
+            final String queryString = getQueryString(state, operation);
+            state.logQuery(operation.getClass().getSimpleName(), queryString);
+            session.execute(queryString);
+        } catch (Exception e) {
+            throw new DbException(e);
+        }
+        resultReporter.report(0, LdbcNoResult.INSTANCE, operation);
     }
 }
