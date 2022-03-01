49bb5d0410193551ce9cd22b6912ba4e114eba40
William Chen <13495049+CPWstatic@users.noreply.github.com>
2022-03-01 17:24:23 +0800

    Impl ic2-14 handlers.

--- nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/NebulaDb.java
+++ nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/NebulaDb.java
@@ -2,10 +2,7 @@ package com.ldbc.impls.workloads.ldbc.snb.nebula;
 
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.control.LoggingService;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1Result;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfileResult;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.*;
 import com.ldbc.impls.workloads.ldbc.snb.db.BaseDb;
 import com.ldbc.impls.workloads.ldbc.snb.nebula.converter.NebulaConverter;
 import com.ldbc.impls.workloads.ldbc.snb.nebula.operationhandlers.NebulaListOperationHandler;
@@ -67,13 +64,8 @@ public class NebulaDb  extends BaseDb<NebulaQueryStore> {
                 // TODO:
             }
 
-            long friendId = 0;
-            if (record.get(0).isString()) {
-                String friendIdStr = record.get(0).asString();
-                // prefix: person-
-                friendId = Long.parseLong(friendIdStr.substring(7));
-            }
-
+            // prefix: person-
+            long friendId = Long.parseLong(record.get(0).asString().substring(7));
             String friendLastName = record.get(1).asString();
             int distanceFromPerson = (int) record.get(2).asLong();
             long friendBirthday = 0;
@@ -98,6 +90,302 @@ public class NebulaDb  extends BaseDb<NebulaQueryStore> {
                     friendCityName,
                     universities,
                     companies);
+        }
+    }
+
+
+    public static class InteractiveQuery2 extends NebulaListOperationHandler<LdbcQuery2, LdbcQuery2Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery2 operation) {
+            return state.getQueryStore().getQuery2(operation);
+        }
+
+        @Override
+        public LdbcQuery2Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            // prefix: person-
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            // TODO: not sure, prefix: message-
+            long messageId = Long.parseLong(record.get(3).asString().substring(8));
+            String messageContent = record.get(4).asString();
+            long messageCreationDate = 0;
+            // TODO: long messageCreationDate = CypherConverter.convertLongTimestampToEpoch(record.get(5).asLong());
+
+            return new LdbcQuery2Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    messageId,
+                    messageContent,
+                    messageCreationDate);
+        }
+    }
+
+    public static class InteractiveQuery3 extends NebulaListOperationHandler<LdbcQuery3, LdbcQuery3Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery3 operation) {
+            return state.getQueryStore().getQuery3(operation);
+        }
+
+        @Override
+        public LdbcQuery3Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            // prefix: person-
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            int xCount = (int) record.get(3).asLong();
+            int yCount = (int) record.get(4).asLong();
+            int count = (int) record.get(5).asLong();
+            return new LdbcQuery3Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    xCount,
+                    yCount,
+                    count);
+        }
+    }
+
+    public static class InteractiveQuery4 extends NebulaListOperationHandler<LdbcQuery4, LdbcQuery4Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery4 operation) {
+            return state.getQueryStore().getQuery4(operation);
+        }
+
+        @Override
+        public LdbcQuery4Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            String tagName = record.get(0).asString();
+            int postCount = (int) record.get(1).asLong();
+            return new LdbcQuery4Result(tagName, postCount);
+        }
+
+    }
+
+    public static class InteractiveQuery5 extends NebulaListOperationHandler<LdbcQuery5, LdbcQuery5Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery5 operation) {
+            return state.getQueryStore().getQuery5(operation);
+        }
+
+        @Override
+        public LdbcQuery5Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            String forumTitle = record.get(0).asString();
+            int postCount = (int) record.get(1).asLong();
+            return new LdbcQuery5Result(forumTitle, postCount);
+        }
+    }
+
+    public static class InteractiveQuery6 extends NebulaListOperationHandler<LdbcQuery6, LdbcQuery6Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery6 operation) {
+            return state.getQueryStore().getQuery6(operation);
+        }
+
+        @Override
+        public LdbcQuery6Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            String tagName = record.get(0).asString();
+            int postCount = (int) record.get(1).asLong();
+            return new LdbcQuery6Result(tagName, postCount);
+        }
+    }
+
+    public static class InteractiveQuery7 extends NebulaListOperationHandler<LdbcQuery7, LdbcQuery7Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery7 operation) {
+            return state.getQueryStore().getQuery7(operation);
+        }
+
+        @Override
+        public LdbcQuery7Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException  {
+            // prefix: person-
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            long likeCreationDate =0;
+            // TODO: long likeCreationDate = CypherConverter.convertLongTimestampToEpoch(record.get(3).asLong());
+            // prefix: message-
+            long messageId = Long.parseLong(record.get(4).asString().substring(8));
+            String messageContent = record.get(5).asString();
+            int minutesLatency = 0;
+            // TODO: int minutesLatency = CypherConverter.convertStartAndEndDateToLatency(record.get(6).asLong(), record.get(3).asLong());
+            boolean isNew = record.get(7).asBoolean();
+            return new LdbcQuery7Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    likeCreationDate,
+                    messageId,
+                    messageContent,
+                    minutesLatency,
+                    isNew);
+        }
+    }
+
+    public static class InteractiveQuery8 extends NebulaListOperationHandler<LdbcQuery8, LdbcQuery8Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery8 operation) {
+            return state.getQueryStore().getQuery8(operation);
+        }
+
+        @Override
+        public LdbcQuery8Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            long commentCreationDate = 0;
+            // TODO: long commentCreationDate = CypherConverter.convertLongTimestampToEpoch(record.get(3).asLong());
+            // prefix: comment-
+            long commentId = Long.parseLong(record.get(4).asString().substring(8));
+            String commentContent = record.get(5).asString();
+            return new LdbcQuery8Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    commentCreationDate,
+                    commentId,
+                    commentContent);
+        }
+    }
+
+    public static class InteractiveQuery9 extends NebulaListOperationHandler<LdbcQuery9, LdbcQuery9Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery9 operation) {
+            return state.getQueryStore().getQuery9(operation);
+        }
+
+        @Override
+        public LdbcQuery9Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            // message-
+            long messageId = Long.parseLong(record.get(3).asString().substring(8));
+            String messageContent = record.get(4).asString();
+            long messageCreationDate = 0;
+            // long messageCreationDate = CypherConverter.convertLongTimestampToEpoch(record.get(5).asLong());
+            return new LdbcQuery9Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    messageId,
+                    messageContent,
+                    messageCreationDate);
+        }
+    }
+
+    public static class InteractiveQuery10 extends NebulaListOperationHandler<LdbcQuery10, LdbcQuery10Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery10 operation) {
+            return state.getQueryStore().getQuery10(operation);
+        }
+
+        @Override
+        public LdbcQuery10Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException {
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            int commonInterestScore = (int) record.get(3).asLong();
+            String personGender = record.get(4).asString();
+            String personCityName = record.get(5).asString();
+            return new LdbcQuery10Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    commonInterestScore,
+                    personGender,
+                    personCityName);
+        }
+    }
+
+    public static class InteractiveQuery11 extends NebulaListOperationHandler<LdbcQuery11, LdbcQuery11Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery11 operation) {
+            return state.getQueryStore().getQuery11(operation);
+        }
+
+        @Override
+        public LdbcQuery11Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException  {
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            String organizationName = record.get(3).asString();
+            int organizationWorkFromYear = (int) record.get(4).asLong();
+            return new LdbcQuery11Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    organizationName,
+                    organizationWorkFromYear);
+        }
+    }
+
+    public static class InteractiveQuery12 extends NebulaListOperationHandler<LdbcQuery12, LdbcQuery12Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery12 operation) {
+            return state.getQueryStore().getQuery12(operation);
+        }
+
+        @Override
+        public LdbcQuery12Result convertSingleResult(ResultSet.Record record) throws UnsupportedEncodingException  {
+            long personId = Long.parseLong(record.get(0).asString().substring(7));
+            String personFirstName = record.get(1).asString();
+            String personLastName = record.get(2).asString();
+            List<String> tagNames = new ArrayList<>();
+            if (!record.get(3).isNull()) {
+                // TODO: tagNames = record.get(3).asList((e) -> e.asString());
+            }
+            int replyCount = (int) record.get(4).asLong();
+            return new LdbcQuery12Result(
+                    personId,
+                    personFirstName,
+                    personLastName,
+                    tagNames,
+                    replyCount);
+        }
+    }
+
+    public static class InteractiveQuery13 extends NebulaSingletonOperationHandler<LdbcQuery13, LdbcQuery13Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery13 operation) {
+            return state.getQueryStore().getQuery13(operation);
+        }
+
+        @Override
+        public LdbcQuery13Result convertSingleResult(ResultSet.Record record) {
+            return new LdbcQuery13Result((int) record.get(0).asLong());
+        }
+    }
+
+    public static class InteractiveQuery14 extends NebulaListOperationHandler<LdbcQuery14, LdbcQuery14Result> {
+
+        @Override
+        public String getQueryString(NebulaDbConnectionState state, LdbcQuery14 operation) {
+            return state.getQueryStore().getQuery14(operation);
+        }
+
+        @Override
+        public LdbcQuery14Result convertSingleResult(ResultSet.Record record) {
+            List<Long> personIdsInPath = new ArrayList<>();
+            if (!record.get(0).isNull()) {
+                // TODO: personIdsInPath = record.get(0).asList((e) -> e.asLong());
+            }
+            double pathWight = record.get(1).asDouble();
+            return new LdbcQuery14Result(
+                    personIdsInPath,
+                    pathWight);
         }
     }
 
--- nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/interactive/NebulaInteractiveDb.java
+++ nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/interactive/NebulaInteractiveDb.java
@@ -2,8 +2,7 @@ package com.ldbc.impls.workloads.ldbc.snb.nebula.interactive;
 
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.control.LoggingService;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.*;
 import com.ldbc.impls.workloads.ldbc.snb.nebula.NebulaDb;
 
 import java.util.Map;
@@ -13,7 +12,21 @@ public class NebulaInteractiveDb extends NebulaDb {
     @Override
     protected void onInit(Map<String, String> properties, LoggingService loggingService) throws DbException {
         super.onInit(properties, loggingService);
+
         registerOperationHandler(LdbcQuery1.class, InteractiveQuery1.class);
+        registerOperationHandler(LdbcQuery2.class, InteractiveQuery2.class);
+        registerOperationHandler(LdbcQuery3.class, InteractiveQuery3.class);
+        registerOperationHandler(LdbcQuery4.class, InteractiveQuery4.class);
+        registerOperationHandler(LdbcQuery5.class, InteractiveQuery5.class);
+        registerOperationHandler(LdbcQuery6.class, InteractiveQuery6.class);
+        registerOperationHandler(LdbcQuery7.class, InteractiveQuery7.class);
+        registerOperationHandler(LdbcQuery8.class, InteractiveQuery8.class);
+        registerOperationHandler(LdbcQuery9.class, InteractiveQuery9.class);
+        registerOperationHandler(LdbcQuery10.class, InteractiveQuery10.class);
+        registerOperationHandler(LdbcQuery11.class, InteractiveQuery11.class);
+        registerOperationHandler(LdbcQuery12.class, InteractiveQuery12.class);
+        registerOperationHandler(LdbcQuery13.class, InteractiveQuery13.class);
+        registerOperationHandler(LdbcQuery14.class, InteractiveQuery14.class);
 
         registerOperationHandler(LdbcShortQuery1PersonProfile.class, ShortQuery1PersonProfile.class);
     }
