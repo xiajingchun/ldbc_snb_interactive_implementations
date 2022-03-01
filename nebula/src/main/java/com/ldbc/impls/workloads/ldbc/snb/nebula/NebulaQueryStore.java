cbd4f2550b449bd55486956a19a3f62cd84d51f7
William Chen <13495049+CPWstatic@users.noreply.github.com>
2022-03-01 18:02:38 +0800

    Impl is2-7 getQueryStr.

--- nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/NebulaQueryStore.java
+++ nebula/src/main/java/com/ldbc/impls/workloads/ldbc/snb/nebula/NebulaQueryStore.java
@@ -45,6 +45,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery5(LdbcQuery5 operation) {
         return prepare(QueryType.InteractiveComplexQuery5, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
@@ -52,6 +53,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery6(LdbcQuery6 operation) {
         return prepare(QueryType.InteractiveComplexQuery6, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
@@ -59,6 +61,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery7(LdbcQuery7 operation) {
         return prepare(QueryType.InteractiveComplexQuery7, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
@@ -66,12 +69,14 @@ public class NebulaQueryStore extends QueryStore {
     }
 
 
+    @Override
     public String getQuery8(LdbcQuery8 operation) {
         return prepare(QueryType.InteractiveComplexQuery8, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
                 .build());
     }
 
+    @Override
     public String getQuery9(LdbcQuery9 operation) {
         return prepare(QueryType.InteractiveComplexQuery9, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
@@ -79,6 +84,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery10(LdbcQuery10 operation) {
         return prepare(QueryType.InteractiveComplexQuery10, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
@@ -87,6 +93,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery11(LdbcQuery11 operation) {
         return prepare(QueryType.InteractiveComplexQuery11, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
@@ -95,6 +102,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery12(LdbcQuery12 operation) {
         return prepare(QueryType.InteractiveComplexQuery12, new ImmutableMap.Builder<String, String>()
                 .put("personId", getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
@@ -102,6 +110,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery13(LdbcQuery13 operation) {
         return prepare(QueryType.InteractiveComplexQuery13, new ImmutableMap.Builder<String, String>()
                 .put("person1Id", getConverter().convertString("person-" + getConverter().convertId(operation.person1Id())))
@@ -109,6 +118,7 @@ public class NebulaQueryStore extends QueryStore {
                 .build());
     }
 
+    @Override
     public String getQuery14(LdbcQuery14 operation) {
         return prepare(QueryType.InteractiveComplexQuery14, new ImmutableMap.Builder<String, String>()
                 .put("person1Id", getConverter().convertString("person-" + getConverter().convertId(operation.person1Id())))
@@ -124,6 +134,55 @@ public class NebulaQueryStore extends QueryStore {
                 QueryType.InteractiveShortQuery1,
                 ImmutableMap.of(
                         LdbcShortQuery1PersonProfile.PERSON_ID, getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
+        );
+    }
+
+
+    @Override
+    public String getShortQuery2PersonPosts(LdbcShortQuery2PersonPosts operation) {
+        return prepare(
+                QueryType.InteractiveShortQuery2,
+                ImmutableMap.of(LdbcShortQuery2PersonPosts.PERSON_ID, getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
+        );
+    }
+
+    @Override
+    public String getShortQuery3PersonFriends(LdbcShortQuery3PersonFriends operation) {
+        return prepare(
+                QueryType.InteractiveShortQuery3,
+                ImmutableMap.of(LdbcShortQuery3PersonFriends.PERSON_ID, getConverter().convertString("person-" + getConverter().convertId(operation.personId())))
+        );
+    }
+
+    @Override
+    public String getShortQuery4MessageContent(LdbcShortQuery4MessageContent operation) {
+        return prepare(
+                QueryType.InteractiveShortQuery4,
+                ImmutableMap.of(LdbcShortQuery4MessageContent.MESSAGE_ID, getConverter().convertString("message-" + getConverter().convertId(operation.messageId())))
+        );
+    }
+
+    @Override
+    public String getShortQuery5MessageCreator(LdbcShortQuery5MessageCreator operation) {
+        return prepare(
+                QueryType.InteractiveShortQuery5,
+                ImmutableMap.of(LdbcShortQuery5MessageCreator.MESSAGE_ID, getConverter().convertString("message-" + getConverter().convertId(operation.messageId())))
+        );
+    }
+
+    @Override
+    public String getShortQuery6MessageForum(LdbcShortQuery6MessageForum operation) {
+        return prepare(
+                QueryType.InteractiveShortQuery6,
+                ImmutableMap.of(LdbcShortQuery6MessageForum.MESSAGE_ID, getConverter().convertString("message-" + getConverter().convertId(operation.messageId())))
+        );
+    }
+
+    @Override
+    public String getShortQuery7MessageReplies(LdbcShortQuery7MessageReplies operation) {
+        return prepare(
+                QueryType.InteractiveShortQuery7,
+                ImmutableMap.of(LdbcShortQuery7MessageReplies.MESSAGE_ID, getConverter().convertString("message-" + getConverter().convertId(operation.messageId())))
         );
     }
 }
