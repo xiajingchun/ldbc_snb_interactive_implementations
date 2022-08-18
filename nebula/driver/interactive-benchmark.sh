750c11814b01b17a4b41452a91db92d448b4fce4
xiajingchun <6269380+xiajingchun@users.noreply.github.com>
2022-08-19 00:15:15 +0800

    moving properties and scripts to v1.2.0

--- nebula/driver/interactive-benchmark.sh
+++ nebula/driver/interactive-benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp ../target/nebula-0.3.5-SNAPSHOT.jar org.ldbcouncil.snb.driver.Client -P interactive-benchmark.properties
+java -cp ../target/nebula-1.0.0.jar org.ldbcouncil.snb.driver.Client -P interactive-benchmark.properties
